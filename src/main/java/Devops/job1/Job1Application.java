18eba647179cb2cd627ad8a08966457d538776c6
Ramesh Nandhyala <89466624+rameshn1990@users.noreply.github.com>
2021-12-27 22:09:32 +0530

    Update Job1Application.java

--- src/main/java/Devops/job1/Job1Application.java
+++ src/main/java/Devops/job1/Job1Application.java
@@ -2,6 +2,7 @@ package Devops.job1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Job1Application {
