67c3c5190b4180c3d80e0ade40c17b61daebd591
Ramesh Nandhyala <89466624+rameshn1990@users.noreply.github.com>
2021-12-27 22:11:49 +0530

    Update Job1Application.java

--- src/main/java/Devops/job1/Job1Application.java
+++ src/main/java/Devops/job1/Job1Application.java
@@ -3,8 +3,12 @@ package Devops.job1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
+
 public class Job1Application {
 	@GetMapping("/message")
 	public String getMessage() {
