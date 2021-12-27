8a1aa3c307c4a074f40bcc35e6afe183f5494160
ramesh <nandhyalaramesh5@gmail.com>
2021-12-27 21:40:09 +0530

    a

---
+++ src/main/java/Devops/job1/Job1Application.java
@@ -0,0 +1,13 @@
+package Devops.job1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job1Application.class, args);
+	}
+
+}
